e6184b9f5ba7bce8e899e1131b0d8383a7cee139
Rosmel <rosmeltics@gmail.com>
2022-03-03 10:01:41 -0500

    Versión 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const d = document,
-    $menuHamburguer = d.querySelector(".menu-hamburguere"),
+    $menuHamburguer = d.querySelector(".menu-hamburguer"),
     $menu = d.querySelector(".menu");
 
 d.addEventListener("click", e => {
+    // ########## MOSTRAR/OCULTAR MENU EN MOVIL ##########
     if (e.target.matches(".menu-hamburguer")) {
         $menu.classList.toggle("menu--visible");
         
@@ -10,6 +11,13 @@ d.addEventListener("click", e => {
             e.target.setAttribute("src", "img/close.svg");
         } else {
             e.target.setAttribute("src", "img/hamburguer.svg");
+        }
+    }
+    // ########## OCULTAR MENU EN MOVIL CON CLICK EN CUALQUIER PARTE DE LA PANTALLA EXCEPTO EN LOS ENLACES##########
+    if($menu.classList.contains("menu--visible")){
+        if (e.target.matches(".menu") && !e.target.matches(".menu__link")) {
+            $menu.classList.remove("menu--visible");
+            $menuHamburguer.setAttribute("src", "img/hamburguer.svg");
         }
     }
 })
